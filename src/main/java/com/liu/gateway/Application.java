4b9386aeccf03ec1c0ba07d1c85844fb9cb4e75c
刘会起 <921300843@qq.com>
2018-07-18 14:56:50 +0800

    init

---
+++ src/main/java/com/liu/gateway/Application.java
@@ -0,0 +1,34 @@
+package com.liu.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by Administrator on 2018/6/14.
+ */
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableCircuitBreaker
+@ComponentScan(basePackages = "com.liu")
+@EnableDiscoveryClient
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+
+    @Bean
+    @LoadBalanced //负载均衡
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+}
